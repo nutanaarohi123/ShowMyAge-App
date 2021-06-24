b77cac74463cfbdeaed68738b07a8377e88fdd15
Nutan <35139185+nutanaarohi123@users.noreply.github.com>
2021-06-24 14:41:32 +0530

    Update ShowAgeActivity.kt

--- app/src/main/java/com/practice/showmyage/ShowAgeActivity.kt
+++ app/src/main/java/com/practice/showmyage/ShowAgeActivity.kt
@@ -11,23 +11,23 @@ import com.practice.showmyage.databinding.ActivityAgeBinding
 class ShowAgeActivity : AppCompatActivity() {
     private lateinit var viewmodel: ShowAgeViewModel
     private lateinit var binding:ActivityAgeBinding
-            @SuppressLint("SetTextI18n")
-            override fun onCreate(savedInstanceState: Bundle?) {
+           
+         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgeBinding.inflate(layoutInflater)
         // Hide the status bar.
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                setContentView(binding.root)
-                viewmodel= ViewModelProvider(this).get(ShowAgeViewModel::class.java)
-         binding.showButton.setOnClickListener {
+        WindowManager.LayoutParams.FLAG_FULLSCREEN)
+          setContentView(binding.root)
+        viewmodel= ViewModelProvider(this).get(ShowAgeViewModel::class.java)
+        binding.showButton.setOnClickListener {
            viewmodel.getMyAge(binding.inputyear)
          }
 
 
          viewmodel.userAge.observe(this,{
             t ->  if(t!=null){
-                val show = "You are ${t.age} year old"
+                val show = R.strings.showmsg                                                           //"You are ${t.age} year old"
              binding.showAge.text = show
              binding.constraintLayout.setBackgroundResource(R.drawable.cupbg2)
              binding.showAge.visibility= View.VISIBLE
